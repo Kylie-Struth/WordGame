318f610eb3a04f3978ab84e39768e183ead6959f
Kylie-Struth <kyliestruth@gmail.com>
2024-01-18 18:27:41 -0700

    Lesson 3: Implemented Hosts, Players, and Turn classes with getters, setters, constructors, and enhanced game logic. GamePlay class now orchestrates the game, prompting the user for their name, instantiating objects, and managing the number guessing game.

---
+++ Players.java
@@ -0,0 +1,18 @@
+public class Players extends Person {
+    private int money;
+    public Players(String firstName, String lastName) {
+        super(firstName, lastName);
+        this.money = 1000;
+    }
+    public int getMoney() {
+        return money;
+    }
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return getFirstName() + (getLastName().isEmpty() ? "" : " " + getLastName()) + ": $" + getMoney() + ".00";
+    }
+}
